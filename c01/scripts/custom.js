2893a8de3ac9fd6789ad7abd69b0d7b35c53f1a5
緒方誠 <ogatamakoto1976@gmail.com>
2017-06-12 23:48:20 +0900

    add page 061

--- c01/scripts/custom.js
+++ c01/scripts/custom.js
@@ -7,4 +7,5 @@ $(function(){
 		$('.external').css('text-decoration', 'none');
 		$('form p').css('border-bottom', '1px solid #ddd');
 		$('tr:nth-child(even)').css('background-color', 'lightblue');
+		$('input[type=text]').css('background-color', 'orange');
 });
