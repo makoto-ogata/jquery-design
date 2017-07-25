fc04c0bb69e7af5ad1f9d390948642c201eb36a0
緒方誠 <ogatamakoto1976@gmail.com>
2017-07-25 21:11:07 +0900

    add page 140-141

--- c03/scripts/custom.js
+++ c03/scripts/custom.js
@@ -13,4 +13,16 @@ $(function(){
 				.children('.content')
 				.slideUp('fast');
 		});
-	});
+
+		$('.dropdown-menu').children('a')
+		.on('click', function(){
+				var $this = $(this);
+				if($this.hasClass('open')) {
+					$this.removeClass('open')
+					.next().hide();
+				} else {
+					$this.addClass('open')
+					.next().show();
+				}
+			});
+});
