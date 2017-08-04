87872af4787b93baf80454d3fbc960ca33679665
Makoto Ogata <ogatamakoto1976@gmail.com>
2017-08-04 20:48:05 +0900

    add page 182-183

--- c03/scripts/custom.js
+++ c03/scripts/custom.js
@@ -81,13 +81,30 @@ $(function(){
 		});
 
 		function changeImage($click){
-			console.log($click);
+			// console.log($click);
+			var $current = $click.siblings('.container').children('.current');
+			var $new;
+			var findSelector = '';
+
+			if($click.hasClass('next')){
+				$new = $current.next();
+				findSelector = ':first-child';
+			} else {
+				$new = $current.prev();
+				findSelector = ':last-child';
+			}
+
+			if($new.length == 0) {
+				$new = $current.siblings(findSelector);
+			}
+			$current.removeClass('current');
+			$new.addClass('current');
 		}
 
 		$('.slide')
 		.on('click', '> a', function(event){
 				event.preventDefault();
 				changeImage($(this));
-			});
+		});
 
 });
