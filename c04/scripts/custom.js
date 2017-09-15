21b2fbabfef07790515de570c21e1e46952b771b
Makoto Ogata <ogatamakoto1976@gmail.com>
2017-09-15 21:04:32 +0900

    add page 214-216

--- c04/scripts/custom.js
+++ c04/scripts/custom.js
@@ -3,12 +3,18 @@ $(function() {
 		function showModal(event){
 			event.preventDefault();
 
+			var $shade = $('<div></div>');
+			$shade
+			.attr('id', 'shade')
+			.on('click', hideModal);
+
 			var $modalWin = $('#modalwin');
 			var $window = $(window);
 			var posX = ($window.width() - $modalWin.outerWidth()) / 2;
 			var posY = ($window.height() - $modalWin.outerHeight()) / 2;
 
 			$modalWin
+			.before($shade)
 			.css({left: posX, top: posY})
 			.removeClass('hide')
 			.addClass('show')
@@ -20,6 +26,7 @@ $(function() {
 		//モーダルウィンドウを閉じる
 
 		function hideModal() {
+			$('#shade').remove();
 			$('#modalwin')
 			.removeClass('show')
 			.addClass('hide');
