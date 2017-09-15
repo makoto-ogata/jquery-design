4625643f75af3e70baf4d1e0452196c46e7ace84
Makoto Ogata <ogatamakoto1976@gmail.com>
2017-09-15 20:43:38 +0900

    add page 211-212

--- c04/scripts/custom.js
+++ c04/scripts/custom.js
@@ -4,8 +4,12 @@ $(function() {
 			event.preventDefault();
 
 			var $modalWin = $('#modalwin');
+			var $window = $(window);
+			var posX = ($window.width() - $modalWin.outerWidth()) / 2;
+			var posY = ($window.height() - $modalWin.outerHeight()) / 2;
 
 			$modalWin
+			.css({left: posX, top: posY})
 			.removeClass('hide')
 			.addClass('show')
 			.on('click', 'button', function() {
