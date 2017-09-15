ae1f60da5a4615d2c0a87c5884c9e84e62018362
Makoto Ogata <ogatamakoto1976@gmail.com>
2017-09-15 20:18:40 +0900

    add page 210

--- c04/scripts/custom.js
+++ c04/scripts/custom.js
@@ -1,44 +1,25 @@
-/**
- * 04-01　モーダルウィンドウ
- */
-$(function(){
-	function showModal() {
-		var $shade = $('<div></div>');
-		$shade
-		.attr('id', 'shade')
-		.on('click', hideModal);
-		
-		var $modalWin = $('#modalwin');
-		var posX = ($(window).width() - $modalWin.outerWidth()) / 2;
-		var posY = ($(window).height() - $modalWin.outerHeight()) / 2;
-		
-		$modalWin
-		.before($shade)
-		.css({left: posX, top: posY})
-		.removeClass('hide')
-		.addClass('show');
-		
-		$modalWin
-		.on('click', '.modal-close', function() {
-			hideModal();
-		})
-		.on('click', 'button', function() {
-			/*
-			 some function
-			 */
-			hideModal();
-		})
-		
-	};
-	
-	function hideModal() {
-		$('#shade').remove();
-		$('#modalwin')
-		.removeClass('show')
-		.addClass('hide');
-	};
+$(function() {
+		//モーダルウィンドウを開く
+		function showModal(event){
+			event.preventDefault();
 
-	$('.show-modal').on('click', showModal);
-	
-	
+			var $modalWin = $('#modalwin');
+
+			$modalWin
+			.removeClass('hide')
+			.addClass('show')
+			.on('click', 'button', function() {
+					hideModal();
+			});
+		};
+
+		//モーダルウィンドウを閉じる
+
+		function hideModal() {
+			$('#modalwin')
+			.removeClass('show')
+			.addClass('hide');
+		};
+
+		$('.show-modal').on('click', showModal);
 });
