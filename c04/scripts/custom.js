7a1215d7a6d52cac053272ac2ba702872d7104d6
Makoto Ogata <ogatamakoto1976@gmail.com>
2017-09-25 02:14:33 +0900

    add page 224-225

--- c04/scripts/custom.js
+++ c04/scripts/custom.js
@@ -36,13 +36,29 @@ $(function() {
 
 		$(window).on('scroll', function(){
 				var scrollValue = $(this).scrollTop();
-				console.log(scrollValue);
+				// console.log(scrollValue);
 				$('.fixedmenu')
 				.trigger('customScroll', { posY: scrollValue});
 		});
 
 		$('.fixedmenu')
+		.each(function(){
+				var $this = $(this);
+				$this.data('initial', $this.offset().top);
+		})
 		.on('customScroll', function(event, object){
-				console.log('customScroll %s', object.posY);
-			});
+				// console.log('customScroll %s', object.posY);
+				var $this = $(this);
+
+				if($this.data('initial') <= object.posY) {
+					if(!$this.hasClass('hixed')) {
+						$this
+						.addClass('fixed')
+						.css({top: 0});
+					}
+				} else {
+					// 要素の固定を解除
+					$this.removeClass('fixed');
+				}
+		});
 });
