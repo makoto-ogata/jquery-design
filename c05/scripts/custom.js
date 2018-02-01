b47771e11b1d6a9097c3562a2952a3762998ab2a
Makoto Ogata <ogatamakoto1976@gmail.com>
2018-02-01 23:13:39 +0900

    add page 264

--- c05/scripts/custom.js
+++ c05/scripts/custom.js
@@ -1,8 +1,31 @@
 $(function(){
+		//テーブルを表示する
+		function renderTable(data){
+			$(data).each(function(){
+					var shop = this;
+
+					var $name = $('<td></td>').text(shop.name);
+					var $address = $('<td></td>').text(shop.address);
+					var $tel = $('<td></td>').text(shop.tel);
+					var $solar = $('<td></td>').text(shop.solar);
+					var $kids = $('<td></td>').text(shop.kids);
+					var $natural = $('<td></td>').text(shop.natural);
+
+					$('<tr></tr>')
+					.append($name)
+					.append($address)
+					.append($tel)
+					.append($solar)
+					.append($kids)
+					.append($natural)
+					.appendTo('#shop');
+				});
+		}
 		// データをロードする
 		var shopdata;
 		$.get('shopdata.json', function(data){
 				shopdata = data;
+				renderTable(shopdata);
 				// console.log(shopdata);
 		})
 		.fail(function(){
